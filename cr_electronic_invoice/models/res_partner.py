65d403efa826a88df2a0618910518c27b01c811d
Ivan Porras <ivanporraslopez@gmail.com>
2025-10-11 08:55:25 -0600

    Update res_partner.py

--- cr_electronic_invoice/models/res_partner.py
+++ cr_electronic_invoice/models/res_partner.py
@@ -32,12 +32,14 @@ class PartnerElectronic(models.Model):
         string="It's export",
         default=False
     )
+    """ Comentado solo para migrar v17
     inscribed = fields.Boolean(
         string="Inscribed",
         default=True,
         readonly=False,
         copy=False
     )
+    """
 
     # === Economic Activity fields === #
 
