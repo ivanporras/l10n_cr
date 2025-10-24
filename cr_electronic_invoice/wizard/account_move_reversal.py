a095ba2016ff08cac582dd2fd6e6cd6c30c33b4b
Ivan Porras <ivanporraslopez@gmail.com>
2025-10-23 20:26:10 -0600

    Update account_move_reversal.py

--- cr_electronic_invoice/wizard/account_move_reversal.py
+++ cr_electronic_invoice/wizard/account_move_reversal.py
@@ -9,8 +9,9 @@ class AccountMoveReversal(models.TransientModel):
     Account move reversal wizard, it cancel an account move by reversing it.
     """
 
-    reference_code_id = fields.Many2one("reference.code", string="Reference Code", required=True)
-    reference_document_id = fields.Many2one("reference.document", string="Reference Document", required=True)
+    company_country_code = fields.Char(related="company_id.country_id.code", string="Company Country Code")
+    reference_code_id = fields.Many2one("reference.code", string="Reference Code", required=False)
+    reference_document_id = fields.Many2one("reference.document", string="Reference Document", required=False)
 
     def _prepare_default_reversal(self, move):
         default_values = super()._prepare_default_reversal(move)
